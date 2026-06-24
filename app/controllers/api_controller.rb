60909a8e62a52c847df3809f275a3f845753a7d4
anim3-sh <animesh@cubeapm.com>
2026-06-24 23:26:53 +0530

    add code for manual sdk instrument and dd-tracer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,16 +4,42 @@ class ApiController < ApplicationController
       end
 
       def param
+        # Concept 1: Custom Tags on Active Span
+        # Retrieve the auto-generated span for this HTTP request
+        span = Datadog::Tracing.active_span
+        if span
+          span.set_tag('my.custom.param', params[:param])
+        end
+
         render plain: "Got param #{params[:param]}"
       end
     
       def exception
-        raise "Sample exception"
+        begin
+          # Simulating a critical failure
+          raise "Sample exception"
+        rescue => e
+          # Concept 2: Manual Error Tracking
+          # If you catch an exception, you must explicitly tell Datadog it failed!
+          span = Datadog::Tracing.active_span
+          span.set_error(e) if span
+          
+          render plain: "Exception occurred and was tracked in Datadog", status: 500
+        end
       end
     
       def api
-        Faraday.get('http://localhost:9000')
-        render plain: "API called"
+        # Concept 3: Creating Custom Spans Programmatically
+        # Wrap specific logic in perfectly timed sub-spans
+        Datadog::Tracing.trace('external.api.call', resource: 'Fetch Localhost') do |span|
+          begin
+            Faraday.get('http://localhost:9000')
+            render plain: "API called"
+          rescue => e
+            span.set_error(e) if span
+            render plain: "API Failed", status: 500
+          end
+        end
       end
 
       def redis
--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,4 +1,18 @@
 Datadog.configure do |c|
-  # Add additional configuration here.
-  # Activate integrations, change tracer settings, etc...
+  # 1. Global Settings
+  # c.service = 'sample_app_ruby_rails'
+  # c.env = 'development'
+  # c.version = '1.0.0'
+
+  # 2. Agentless Configuration (Send directly to CubeAPM) from the code.
+  # c.agent.host = 'host.docker.internal'
+  # c.agent.port = 3130
+
+  # 3. Manual Plugin Instrumentation
+  # If you remove "require: 'datadog/auto_instrument'" from your Gemfile,
+  # you must manually enable plugins here:
+  # c.tracing.instrument :rails
+  # c.tracing.instrument :redis
+  # c.tracing.instrument :faraday
+  # c.tracing.instrument :mysql2
 end
